f5c5f4c5f7b12d9bdc8aebef7543ee9ab5ee0350
Ivan Kashtanov <ivankashtanov@Ivans-MacBook-Pro.local>
2022-03-23 16:38:17 +0300

    refactor

--- docs/firebase-messaging-sw.js
+++ docs/firebase-messaging-sw.js
@@ -17,7 +17,16 @@
   // messages.
 const messaging = firebase.messaging();
 **/
-
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("../firebase-messaging-sw.js")
+    .then(function (registration) {
+      console.log("Registration successful, scope is:", registration.scope);
+    })
+    .catch(function (err) {
+      console.log("Service worker registration failed, error:", err);
+    });
+}
 importScripts(
   "https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js"
 );
